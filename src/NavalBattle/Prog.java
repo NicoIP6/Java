cabd464ab6e308efc34c8ca8ad9fb9087a5587ae
ancio <ancion.nico@gmail.com>
2023-09-26 18:35:57 +0200

    1er cours Java

---
+++ src/NavalBattle/Prog.java
@@ -0,0 +1,25 @@
+//This is a comment
+package NavalBattle;
+
+public class Prog {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+
+        //Déclaration et initialisation de J1_points
+        var J1_Points = 0;
+        System.out.println(J1_Points);
+        /*
+            Idem pour J2
+            Nombre de point J2
+         */
+        var J2_Points = 0;
+
+        //Déclaration d'une constante
+        final var MAX_POINTS = 5 + 4 + 3 + 3 + 2;
+
+        //Création d'un tableau pour la grille d'attaque
+        var grilleAttaque = new char[100];
+        // une autre façon de déclarer un tableau : int[] tabEntier = {1, 2, 3, 4, 5, 6, 7};
+
+    }
+}
